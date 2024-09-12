1f04150c04b8444a196efc540f0dc0b60cdbfac9
daffarizqanfairuz <daffarizqanfairuz@gmail.com>
2024-09-12 22:52:32 +0700

    Menambahkan Profile Controller dan View Profile

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -7,12 +8,10 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can register web routes for your application. Thesed
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/profile/{nama}/{kelas}/{npm}', [ProfileController::class, 'profile']);
