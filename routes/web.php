fb16e09234dd54522013e0c577d8bd1c17a7ec03
daffarizqanfairuz <daffarizqanfairuz@gmail.com>
2024-09-12 21:39:38 +0700

    mengubah profile

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +12,4 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/profile/{nama}/{kelas}/{npm}', [ProfileController::class, 'profile']);
