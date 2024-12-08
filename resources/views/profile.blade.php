c89dc5b415c1ce2db257b501bf12aa1f2fc71f8f
daffarizqanfairuz <daffarizqanfairuz@gmail.com>
2024-12-08 20:39:50 +0700

    form dan controller

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -7,19 +7,30 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <div class="flex flex-col items-center justify-center h-screen">
-        <div class="w-40 h-40 bg-gray-300 rounded-full flex items-center justify-center mb-6">
-            <img src="{{ asset('walawe/img/suisei.png') }}" alt="Profile Picture" class="rounded-full">
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kartu Profil</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/profile.css') }}">
+</head> 
+<body>
+    <div class="header">
+        <h1>Profil Mahasiswa</h1>
+    </div>
+    <div class="card">
+        <div class="card-image">
+        <img src="{{ asset('assets/img/download.jpeg') }}" alt="Deskripsi Gambar" class="rounded-full">
         </div>
-        <div class="w-full max-w-xs">
-            <div class="bg-gray-100 p-4 mb-4 text-center rounded-lg">
-                <p class="font-semibold text-lg"><?= $nama ?></p>
-            </div>
-            <div class="bg-gray-100 p-4 text-center rounded-lg">
-                <p class="font-semibold text-lg"><?= $npm ?></p>
-            </div>
-            <div class="bg-gray-100 p-4 mb-4 mt-4 text-center rounded-lg">
-                <p class="font-semibold text-lg"><?= $kelas ?? 'Kelas tidak ditemukan'?></p>
+        <div class="card-content">
+            <div class="bg-gray-100 p-2 mb-2 text-center rounded-lg">
+            <p class=><?= $nama ?></p>
+            <p class="mt-2 mb-2" ></p>
+            <p class=><?= $npm ?></p>
+            <p class="mt-2 mb-2"></p>
+            <p class=> <?$kelas ?? 'Kelas tidak ditemukan'?>Kelas A</p>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,8 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/profile/{nama}/{kelas}/{npm}', [ProfileController::class, 'profile']);
-
+Route::get('/user/create', [UserController::class, 'create']);
+Route::get('/user/profile', [UserController::class, 'profile']);
+Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
 
 Route::get('/', function () {
     return view('welcome');
